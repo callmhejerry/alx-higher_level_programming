b53a151782124a77ceba141538daf37daf253817
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-11-30 17:38:55 +0100

    added 9-print_last_digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    if (number >= 0):
+        last_digit = number % 10
+    else:
+        last_digit = -(number) % 10
+    print(last_digit, end="")
+    return last_digit
+
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
