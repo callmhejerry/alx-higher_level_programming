31b4cdb10a5eb27f2380335fff67d3e68f489503
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-24 20:48:17 +0100

    added 2-args.py file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args_list = sys.argv
+    args_len = len(args_list) - 1
+    if args_len > 0:
+        print("{:d} arguments:".format(args_len))
+        for count in range(1, args_len + 1):
+            print("{:d}: {}".format(count, args_list[count]))
+    else:
+        print("{:d} arguments.".format(args_len))
