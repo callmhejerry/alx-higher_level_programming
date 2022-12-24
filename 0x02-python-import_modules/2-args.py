ee2e39fc414b657e54f84d5957e39b36a0faad4f
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-24 20:54:44 +0100

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,12 @@ import sys
 if __name__ == "__main__":
     args_list = sys.argv
     args_len = len(args_list) - 1
-    if args_len > 0:
+    if args_len < 1:
         print("{:d} arguments:".format(args_len))
+    else:
+        if args_len == 1:
+            print("{:d} argument:".format(args_len))
+        else:
+            print("{:d} arguments:".format(args_len))
         for count in range(1, args_len + 1):
             print("{:d}: {}".format(count, args_list[count]))
-    else:
-        print("{:d} arguments.".format(args_len))
