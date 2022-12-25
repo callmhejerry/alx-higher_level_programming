96452482fad3eeac8e03914e0a4730df93a93312
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-25 12:53:01 +0100

    added 103-fast_Alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'].__str__()
-print(alpha)
+import string
+print(string.ascii_uppercase)
