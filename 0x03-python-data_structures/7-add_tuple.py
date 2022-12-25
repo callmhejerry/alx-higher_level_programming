06997cdc32fe16e1250cd83e3017e6de321cda0c
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-25 19:50:37 +0100

    hot fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,13 +5,13 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) >= 2:
-        a1, a2 = tuple_a
+        a1, a2 = tuple_a[0], tuple_a[1]
     elif len(tuple_a) == 1:
         a1, a2 = tuple_a[0], 0
     else:
         a1, a2 = 0, 0
     if len(tuple_b) >= 2:
-        b1, b2 = tuple_b
+        b1, b2 = tuple_b[0], tuple_b[1]
     elif len(tuple_b) == 1:
         b1, b2 = tuple_b[0], 0
     else:
