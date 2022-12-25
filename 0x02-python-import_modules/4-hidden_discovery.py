f929d348f20584422ddeab1a41e47f6b0fff9815
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-25 12:50:07 +0100

    added 4-hidden_discover.py file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-import dis
+if __name__ == '__main__':
+    import hidden_4
 
+    name_list = dir(hidden_4)
+
+    for name in name_list:
+        if name[1] != '_':
+            print(name);
