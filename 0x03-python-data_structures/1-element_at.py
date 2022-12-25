2ea7a0b4f5d5a6c8026ca78893399211f27aa3ea
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-25 14:28:41 +0100

    added 1-element_at.py file

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+'''A fucntion that retrieves an element from a list'''
+
+def element_at(my_list, idx):
+    if idx < 0 and idx >= len(my_list):
+        return None
+    return my_list[idx]
