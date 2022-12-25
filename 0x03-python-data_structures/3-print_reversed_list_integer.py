359c120558dcadc864a26c812cae0d7ca51609c8
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-25 18:34:53 +0100

    hot fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,6 @@
     in reverse order'''
 
 
-def print_reversed_list_integers(my_list=[]):
+def print_reversed_list_integer(my_list=[]):
     for idx in range(len(my_list) - 1, -1, -1):
         print("{:d}".format(my_list[idx]))
