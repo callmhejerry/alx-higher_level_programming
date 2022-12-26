d8316993d531fc7959f15c1fcf7602bbdea42957
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-26 13:00:48 +0100

    Added 7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''A function that replaces or adds key/value
+    in a dictionary'''
+
+
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary.update({key: value})
+    return a_dictionary
