49ae2cd13f67bcac33870f5680ebd6dc7477bf14
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-27 13:35:40 +0100

    added 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''A fucniton that converts a roman numeral to an integer'''
+
+
+def roman_to_int(roman_string):
+    if roman_string is None or not(isinstance(roman_string, str)):
+        return 0
+    roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    num = 0
+    for letter in roman_string.upper():
+        num += roman[letter]
+    return num
