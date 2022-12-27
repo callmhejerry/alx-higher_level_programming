c6ebfe3d346beb4e99fc5d50c7802f7107992d3f
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-27 17:21:52 +0100

    added 100-weight_average.py file

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+'''A function thart returns the weighted average of all
+    integers tuple'''
+
+
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    weight_mul = 0
+    weighted_sum = 0
+
+    for tuple in my_list:
+        score , weight = tuple
+        weight_mul += score * weight
+        weighted_sum += score
+    weighted_average = weight_mul / weighted_sum
+    return weighted_average
