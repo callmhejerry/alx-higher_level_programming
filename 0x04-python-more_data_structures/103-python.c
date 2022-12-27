8fd028cd7aae5e0c60104b8fc45a8104d61524e4
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-28 00:03:09 +0100

    hot fix

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -14,21 +14,21 @@ void print_python_bytes(PyObject *p)
     pyByte = (PyBytesObject *)p;
     size = pyByte->ob_base.ob_size;
     byteString = pyByte->ob_sval;
-    printf("[.] bytes object info");
-    if (PyBytes_Check(pyByte))
+    printf("[.] bytes object info\n");
+    if (!PyBytes_Check(pyByte))
     {
-        printf("[ERROR] Invalid Bytes Object");
+        printf("[ERROR] Invalid Bytes Object\n");
         return;
     }
-    printf("  size: %li", size);
-    printf("  trying string: %s", byteString);
+    printf("  size: %li\n", size);
+    printf("  trying string: %s\n", byteString);
     if (size >= 10)
         max = 10;
     else
         max = size + 1;
-    printf("  first %li bytes: ");
+    printf("  first %li bytes: ", max);
     for (count = 0; count < max; count++){
-        printf("%2x", byteString[count]);
+        printf("%02x", byteString[count]);
         if (count + 1 == max)
             printf("\n");
         else
@@ -44,23 +44,24 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_list(PyObject *p)
 {
-    PyListObject *pyList, item;
+    PyListObject *pyList;
     PyTypeObject pytype;
+    PyObject item;
     Py_ssize_t size, allocated, count;
 
     pyList = (PyListObject *)p;
     size = pyList->ob_base.ob_size;
     allocated = pyList->allocated;
 
-    printf("[*] Python list info");
-    printf("[*] Size of the Python list = %li", size);
-    printf("[*] Allocated = %li", allocated);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python list = %li\n", size);
+    printf("[*] Allocated = %li\n", allocated);
     for (count = 0; count < size; count++)
     {
-        item = (pyList->ob_item)[count];
-        pytype = (PyTypeObject)item;
-        printf("Element %li: %s", count, pytype.tp_name);
-        if (strcmp(pytype.tp_name, "bytes"))
-            print_python_bytes(item);
+        item = *(pyList->ob_item)[count];
+        pytype = *item.ob_type;
+        printf("Element %li: %s\n", count, pytype.tp_name);
+        if (strcmp(pytype.tp_name, "bytes") == 1)
+            print_python_bytes(&item);
     }
 }
