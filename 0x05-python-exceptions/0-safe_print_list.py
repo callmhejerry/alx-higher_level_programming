27b8777d91e028fedfaad53859f5f2f6e1d22320
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-30 18:08:59 +0100

    added 1-safe_print_integer.py file anf did a bug fix to 0-safe_print_list.py file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,6 +10,6 @@ def safe_print_list(my_list=[], x=0):
         for idx in range(0, x):
             print("{}".format(my_list[idx]), end="")
             num_printed += 1
-    except:
+    except Exception:
         print("")
     return num_printed
