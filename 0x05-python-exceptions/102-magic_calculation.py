02d3accc58877d45988ddddfc7e01ef8e1c7d899
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2022-12-31 11:35:38 +0100

    Added 102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                b = a ** b
+                i = b / i
+                i = result + i
+                result = i
+        except:
+            a = a + b
+            result =  a
+            break
+    return result
