1a03b7d36c9a39b0453c19416022a1aba75e3222
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-01-02 20:17:38 +0100

    Added 6-rectangle.py file

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@
 class Rectangle:
     '''A Rectangle class'''
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         '''initializes the Rectangle class'''
         self.width = width
