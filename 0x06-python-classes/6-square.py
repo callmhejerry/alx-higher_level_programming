14f4008f9727d80c31c969f3bfdbe2c2f053ea1a
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-01-02 12:31:46 +0100

    hot fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,6 +53,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(0, self.__size):
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
                 print(" "*self.__position[0], end="")
                 print("#"*self.__size)
