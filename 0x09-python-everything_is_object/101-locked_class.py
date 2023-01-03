060c94b4459d6598b853a83fdfc8fce84b6bbf96
root <root@DESKTOP-93H7LGT>
2023-01-04 00:08:49 +0100

    hot fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -10,12 +10,8 @@ class LockedClass:
         if __name != "first_name":
             raise AttributeError("'LockedClass'\
  object has no attribute '{}'".format(__name))
-        self.first_name = __value
+        self.__dict__[__name] = __value
 
     @property
     def first_name(self):
         return self.first_name
-
-    @first_name.setter
-    def first_name(self, value):
-        self.first_name = value
