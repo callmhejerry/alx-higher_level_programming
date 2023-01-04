67b8e653f791ebe947f76bbc9755be27021d53dd
root <root@DESKTOP-93H7LGT>
2023-01-04 20:19:09 +0100

    Added 0-add_integer.py and 0-add_integer.txt test file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''
+A function that adds two numbers
+a - the first argument
+b - the second argument with a default value of 98
+'''
+
+
+def add_integer(a, b=98):
+    '''
+    returns the addition of a and b
+    '''
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+
+    return a + b
