b626a9c7c4c3b6a3626e6d4e7d15133d1f432d20
callmhejerry <chinedujeremiah2002@gmail.com>
2023-01-06 12:49:34 +0100

    Added 4-print_square.py file

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,3 +15,11 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
+
+say_my_name("John", "Smith")
+say_my_name("Walter", "White")
+say_my_name("Bob")
+try:
+    say_my_name(12, "White")
+except Exception as e:
+    print(e)
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+'''
+A function that prints a square with the character #
+@size: the size of the square to print
+'''
+
+
+def print_square(size):
+    '''prints a square with the character #'''
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#"*size)
