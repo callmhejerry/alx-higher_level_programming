2aa49220e4002d8d8008a5275401484bfc2ae15b
callmhejerry <chinedujeremiah2002@gmail.com>
2023-01-09 21:37:35 +0100

    hot fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -23,4 +23,5 @@ class Rectangle(BaseGeometry):
         return self.__height * self.__width
 
     def __str__(self) -> str:
-        return "[{}] {}/{}".format(type(self).__name__, self.__width, self.__height)
+        return "[{}] {}/{}".format(type(self).__name__,
+                                   self.__width, self.__height)
