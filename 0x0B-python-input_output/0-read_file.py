fdb4b987ed39ecb9e4ecdb1dad133b9caa954943
callmhejerry <chinedujeremiah2002@gmail.com>
2023-01-10 20:27:17 +0100

    hot fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ a text file and prints it to standard output
 def read_file(filename=""):
     '''reads the contents in a file [filename] and prints in stdout'''
     with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line)
+        content = file.read()
+        print(content)
