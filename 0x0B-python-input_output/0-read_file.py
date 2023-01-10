1361aa0557547dc1b8da600b6ec08865558cfa66
callmhejerry <chinedujeremiah2002@gmail.com>
2023-01-10 20:09:26 +0100

    hot fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,10 +4,10 @@
 This Module defines a function that reads
 a text file and prints it to standard output
 '''
-import sys
+
 
 def read_file(filename=""):
     '''reads the contents in a file [filename] and prints in stdout'''
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
-            sys.stdout.write(line)
+            print(line)
