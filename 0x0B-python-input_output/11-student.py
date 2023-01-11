0623822d7c5cabef302e4f1c447b2a0d72b6a2d8
callmhejerry <chinedujeremiah2002@gmail.com>
2023-01-11 08:19:35 +0100

    Added 11-student.py file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+'''
+This Module defines a Student class
+'''
+
+
+class Student():
+    '''Student class'''
+    def __init__(self, first_name, last_name, age):
+        '''creates the student object'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Return the dictionary representation of student object'''
+        if attrs is None:
+            return self.__dict__
+        my_dict = {}
+        for key in attrs:
+            if hasattr(self, key):
+                my_dict[key] = self.__dict__[key]
+        return my_dict
+
+    def reload_from_json(self, json):
+        self.age = json["age"]
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
