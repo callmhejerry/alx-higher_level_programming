88742855cbf0d25ba56ffe3f3706ecab4491d459
callmhejerry <chinedujeremiah2002@gmail.com>
2023-01-12 09:15:06 +0100

    added base.py file

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''
+This Module defines a base [base] class 
+'''
+
+
+class Base():
+    '''A Base class'''
+    __nb_objects = 0
+
+    def __init__(self, id=None) -> None:
+        '''creates the Base class instance'''
+        if id is None:
+            self.id = id
+        else:
+            Base.__nb_objects +=1
+            self.id = Base.__nb_objects
