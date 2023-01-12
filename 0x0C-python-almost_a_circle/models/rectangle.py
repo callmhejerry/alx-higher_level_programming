64c375786afa531752482ec674be9430d46433c3
callmhejerry <chinedujeremiah2002@gmail.com>
2023-01-12 09:40:05 +0100

    added getter and setters for the attributes of the Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,7 +15,67 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         '''creates the Rectangle class instance'''
         Base.__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        '''returns the width property'''
+        return self.__width
+
+    @property
+    def height(self):
+        '''returns the height property'''
+        return self.__height
+
+    @property
+    def x(self):
+        '''returns the x property'''
+        return self.__x
+
+    @property
+    def y(self):
+        '''returns the y property'''
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        '''sets the private width property'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be greater than 0") 
+        self.__width = value
+    
+    @height.setter
+    def height(self, value):
+        '''sets the private height property'''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be greater than 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        '''sets the private x property'''
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value <= 0:
+            raise ValueError("x must be greater than 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        '''sets the private y property'''
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value <= 0:
+            raise ValueError("y must be greater than 0")
+        self.__y = value
+
+    def area(self):
+        '''returns the area of the Rectangle'''
+        return self.height * self.width
