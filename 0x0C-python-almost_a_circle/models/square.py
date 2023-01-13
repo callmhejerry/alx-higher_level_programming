61ec462c1e6125c78d799a58392f50ed8627db3c
callmhejerry <chinedujeremiah2002@gmail.com>
2023-01-13 08:49:34 +0100

    Added the setter method for size in the Sqaure class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,3 +19,12 @@ class Square(Rectangle):
         '''Returns the string representation of the Sqaure'''
         return "[Square] ({:d}) {:d}/{:d} - {:d}"\
             .format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
