4abfced54eb7bda09a35e0b3b01ea52f279856c7
callmhejerry <chinedujeremiah2002@gmail.com>
2023-01-18 12:08:24 +0100

    hot fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,7 +14,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         '''creates the Rectangle class instance'''
-        Base.__init__(self, id)
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
