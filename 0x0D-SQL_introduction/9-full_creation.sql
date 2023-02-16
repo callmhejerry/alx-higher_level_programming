98d8f026916b1aa5619aef9f1820f543b597729a
callmhejerry <chinedujeremiah2002@gmail.com>
2023-02-16 15:20:32 +0100

    hot fix

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS second_table
 );
 
 INSERT INTO second_table (id, name, score)
-VALUES (1, 'john', 10);
+VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score)
 VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, score)
