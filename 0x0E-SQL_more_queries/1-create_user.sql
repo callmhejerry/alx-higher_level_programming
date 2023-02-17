f85f72fb5735f56a34652c6d98576cf69c0a4abb
callmhejerry <chinedujeremiah2002@gmail.com>
2023-02-17 10:27:00 +0100

    hot fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,7 @@
 -- A script that creates the MySQL user
-CREATE USER IF NOT EXISTS user_0d_1@localhost
-IDENTIFIED BY 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL
-TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES
+ON *.*
+TO 'user_0d_1'@'localhost';
