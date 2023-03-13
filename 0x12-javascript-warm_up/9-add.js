20f4ed78d73d2b96b13e61c3aa9ab27999dfdc1f
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-13 12:16:09 +0100

    added 9-add.js file

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/bin/node
+
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
+
+function add (a, b) {
+  const result = first + second;
+  console.log(result);
+}
+
+add(first, second);
