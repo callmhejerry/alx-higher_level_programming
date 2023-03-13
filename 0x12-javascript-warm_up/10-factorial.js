5a467328be3386bc42f997e49baa481ddbe57876
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-13 12:24:02 +0100

    Added 10-factorial.js file

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/bin/node
+
+const arg = parseInt(process.argv[2]);
+
+function factorial (num) {
+  if (Number.isNaN(num) || num === 0) return 1;
+
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(arg));
