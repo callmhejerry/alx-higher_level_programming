1af848d91c6201561d1eef795909fb2fa95554a6
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-13 11:35:02 +0100

    added 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/bin/node
+
+const xTimes = parseInt(process.argv[2]);
+
+if (xTimes) {
+  for (let i = 0; i < xTimes; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurences');
+}
