f6eb37b7a99a8b265de962b66f89d3acccd792fc
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-13 10:53:39 +0100

    Added 2-arguments.js file

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,7 @@
+#!/bin/node
+
+const lengthOfArgument = process.argv.length;
+
+if (lengthOfArgument < 3) console.log('No argument');
+if (lengthOfArgument === 3) console.log('Argument found');
+if (lengthOfArgument > 3) console.log('Arguments found');
