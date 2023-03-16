1424f75f445703895e91b3103541d7dd7c5ce145
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-16 18:40:37 +0100

    added my_safe_filter_states.py file

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+'''
+A script that takes in an arguement and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+'''
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    args = sys.argv[4]
+
+    db = MySQLdb.Connect(host="localhost", port=3306,
+                         user=username, passwd=password,
+                         db=database)
+    cur = db.cursor()
+    cur.execute('''
+                SELECT * FROM states
+                WHERE name = '{}'
+                ORDER BY id
+                '''.format(args))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
