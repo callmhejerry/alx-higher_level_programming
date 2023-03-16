7a2e3c74983113975ddeecd1c7cb6a640af5d91f
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-16 22:44:41 +0100

    Added 10-model_state_my_get.py file

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+'''
+A script that prints the State object with the name
+passed as argumment from the database hbtn_0e_6_usa
+'''
+
+
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    import sys
+    from sqlalchemy.orm import sessionmaker
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+    db_url = "mysql://{}:{}@localhost:3306/{}".\
+             format(username, password, database)
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    objs = session.query(State).filter(State.name == name).all()
+    if len(objs) == 0:
+        print("Not found")
+    else:
+        print(objs[0].id)
