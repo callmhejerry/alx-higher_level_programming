ce18e57e47b760a9181062019b88123111a74899
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-31 16:43:22 +0100

    Added 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that sends a JSON POST request to a url
-curl -s -X POST -H 'Content-Type: application/json' --data-binary "$2" "$1"
+curl -s -X POST -H 'Content-Type: application/json' --data-binary @"$2" "$1"
