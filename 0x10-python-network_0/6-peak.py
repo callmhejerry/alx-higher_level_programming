dcae675316a46189b9420dae4c697a413f59515f
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-31 14:15:24 +0100

    modified the find_peak function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,7 +13,8 @@ def find_peak(list_of_integers):
     
     if len(list_of_integers) == 1:
         return list_of_integers[0]
-    
+    if len(list_of_integers) == 2:
+        return max(list_of_integers[0], list_of_integers[1])
     mid = len(list_of_integers) // 2
     left_peak = find_peak(list_of_integers[:mid])
     right_peak = find_peak(list_of_integers[mid:])
