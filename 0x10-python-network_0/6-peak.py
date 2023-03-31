6479402c6bf661548558ea1d0139494d199e378e
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-31 16:03:57 +0100

    fix style error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,7 +10,6 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
-    
     if len(list_of_integers) == 1:
         return list_of_integers[0]
     if len(list_of_integers) == 2:
@@ -18,8 +17,6 @@ def find_peak(list_of_integers):
     mid = len(list_of_integers) // 2
     left_peak = find_peak(list_of_integers[:mid])
     right_peak = find_peak(list_of_integers[mid:])
-    
     if left_peak > right_peak:
         return left_peak
     return right_peak
-    
