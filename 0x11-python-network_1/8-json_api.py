cf17ab44546476cd22816be41f52e7163671fec3
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-02 15:48:50 +0100

    hot fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -26,5 +26,5 @@ if __name__ == "__main__":
                 id = json_response.get('id')
                 name = json_response.get('name')
                 print("[{}] {}".format(id, name))
-    except requests.exceptions.JSONDecodeError:
+    except Exception:
         print("Not a valid JSON")
