2d7d69c1949d36c201f5c040f5eca6628f32eed3
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-02 11:26:02 +0100

    Added 3-error_code.py file

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''
+A script that takes in a url,sends a request to the url and
+displays the body of the response
+'''
+
+
+if __name__ == "__main__":
+    import sys
+    import urllib.error
+    import urllib.request
+
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            body = body.decode("utf-8")
+            print(body)
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
