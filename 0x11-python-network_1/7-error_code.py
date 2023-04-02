9037991185df03e8cccf5fa0ff27932990a31f9d
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-02 13:24:29 +0100

    Added 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+'''
+A script that takes in a url, sends a request to the url
+and displays the body of the response
+'''
+
+
+if __name__ == " __main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    with requests.get(url) as response:
+        code = response.status_code
+        if code == 200:
+            print(response.text)
+        if code >= 400:
+            print("Error code:", code)
