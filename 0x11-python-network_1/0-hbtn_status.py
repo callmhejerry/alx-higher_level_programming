3497264f486b93a8b20caaa3f204a08b563462f6
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-02 10:48:35 +0100

    code style fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ A Script that fetches https://alx-intranet.hbtn.io/status
 if __name__ == "__main__":
     import urllib.request
     import urllib.parse
-    
+
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         data = response.read()
