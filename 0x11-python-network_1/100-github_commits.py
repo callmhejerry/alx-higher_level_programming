85436259ed70d52128fbe76c38faca1540625ac7
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-02 16:28:28 +0100

    Added 100-github_commits.py file

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''
+A script that 
+'''
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    headers = {'X-GitHub-Api-Version': '2022-11-28', "accept": "application/vnd.github+json"}
+    params = {
+        "per_page" : 10,
+    }
+
+    with requests.get(url, headers=headers, params=params) as response:
+        if response.status_code == 200:
+            json_reponse = response.json()
+            for item in json_reponse:
+                sha = item.get('sha')
+                name = item.get('commit').get('author').get('name')
+                print("{}: {}".format(sha, name))
