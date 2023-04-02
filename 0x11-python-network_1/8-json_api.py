fc773251094c66dd0ec28bdf44ed304dd75f4fd4
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-02 14:26:47 +0100

    Added 8-json_api.py file

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+'''
+A script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+'''
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {}
+    try:
+        param = sys.argv[1]
+        data = {"q": param}
+    except Exception:
+        data = {"q": ""}
+    try :
+        with requests.post(url, data=data) as response:
+            json_response = response.json()
+            if json_response == {}:
+                print("No result")
+            else:
+                id = json_response.get('id')
+                name = json_response.get('name')
+                print("[{}] {}".format(id, name))
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
