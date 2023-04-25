4a45eddac87d8512da7a26083f9618012bba6bf7
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-25 17:51:37 +0100

    Added 6-completed_tasks.js file

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  if (body) {
+    const obj = JSON.parse(body);
+    const data = {};
+    for (const todo of obj) {
+      if (todo.userId in data) {
+        if (todo.completed) {
+          data[todo.userId] += 1;
+        }
+      } else {
+        data[todo.userId] = 0;
+        if (todo.completed) {
+          data[todo.userId] += 1;
+        }
+      }
+    }
+    console.log(data);
+  }
+});
