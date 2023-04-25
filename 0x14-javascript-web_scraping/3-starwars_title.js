7ae4d11bb775cfc13685b4b0c154ef197d539685
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-25 16:11:42 +0100

    added 3-starwars_title.js file

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,12 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url).on('data', (data) => {
-  const obj = JSON.parse(data.toString());
-  console.log(obj.title);
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  if (body) {
+    const obj = JSON.parse(body);
+    console.log(obj.title);
+  }
 });
