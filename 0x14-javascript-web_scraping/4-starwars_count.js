9d147cda10dfd03b620e0f811a7b3137af4cc852
callmhejerry <chinedujeremiah2002@example.com>
2023-04-26 10:34:22 +0000

    hot fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,24 @@ const request = require('request');
 
 const url = process.argv[2];
 
+
+function findCharacter(characters) {
+  for (let character of characters) {
+    if (character.includes('18')) {
+      return true;
+    }
+  }
+}
+
 request.get(url, (err, res, body) => {
   if (err) {
     console.log(err);
   }
   if (body) {
     const obj = JSON.parse(body);
-    const character = 'https://swapi-api.alx-tools.com/api/people/18/';
+    // const character = 'https://swapi-api.alx-tools.com/api/people/18/';
     const newArr = obj.results.filter((value) => {
-      return value.characters.includes(character);
+      return findCharacter(value.characters);
     });
     console.log(newArr.length);
   }
