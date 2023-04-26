600f44cc30fccd9d2811f2cf71273aa2900b7e71
callmhejerry <chinedujeremiah2002@example.com>
2023-04-26 10:44:02 +0000

    hot fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,13 +11,11 @@ request.get(url, (err, response, body) => {
     const obj = JSON.parse(body);
     const data = {};
     for (const todo of obj) {
-      if (todo.userId in data) {
-        if (todo.completed) {
+      if (todo.completed) {
+        if (todo.userId in data) {
           data[todo.userId] += 1;
-        }
-      } else {
-        data[todo.userId] = 0;
-        if (todo.completed) {
+        } else {
+          data[todo.userId] = 0;
           data[todo.userId] += 1;
         }
       }
